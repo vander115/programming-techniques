72dff578234e4bc9bb9efd7628d8c6d45a189ad9
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-09-02 09:56:01 -0300

    refactor: change exerc01

--- src/com/vander/list01/Exercise01.java
+++ src/com/vander/list01/Exercise01.java
@@ -20,12 +20,12 @@ public class Exercise01 {
 
 		if (A > 10) {
 			System.out.println("A > 10");
-		}
 
-		if (A + B == 20) {
-			System.out.println("A + B == 20");
-		} else {
-			System.out.println("Número não válido!");
+			if (A + B == 20) {
+				System.out.println("A + B == 20");
+			} else {
+				System.out.println("Número não válido!");
+			}
 		}
 
 	}
